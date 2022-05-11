9473d2d204ea79f932b1d0a73836369b39df8a81
Alex <skorpion_94@list.ru>
2022-05-11 15:25:14 +0300

    Минимум и максимум двух чисел

---
+++ work02/Program.cs
@@ -0,0 +1,21 @@
+﻿
+Console.WriteLine("Write_number_1:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Write_number_2:");
+int b = int.Parse(Console.ReadLine());
+int max = a;
+int min = b;
+if (b>max)
+{
+    Console.Write("Max = ");
+    Console.WriteLine(max = b);
+    Console.Write("Min = ");
+    Console.WriteLine(min = a);
+}
+else
+{
+    Console.Write("Max = ");
+    Console.WriteLine(max = a);
+    Console.Write("Min = ");
+    Console.WriteLine(min = b);
+}
