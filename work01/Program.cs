3a33372d375d05f0d2a6341d865d475174b631c2
Alex <skorpion_94@list.ru>
2022-05-11 14:55:29 +0300

    Задача на нахождение квадрата числа(Parsel)

---
+++ work01/Program.cs
@@ -0,0 +1,18 @@
+﻿// Первый вариант
+//int a = 5;
+//int B = 0;
+//B = a * a;
+//Console.Write(B);
+
+// Второй вариант
+//int a = 5;
+//Console.Write("Квадрат числа:");
+//Console.WriteLine(a * a);
+
+// Третий варинт
+Console.WriteLine("Введи число:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine(a * a);
+
+
+
