0276ce65ad08d70f7107a2e9a5d4c8ea1294aef3
Alex <skorpion_94@list.ru>
2022-05-11 16:20:17 +0300

    Минимум и максимум трех чисел

---
+++ work03/Program.cs
@@ -0,0 +1,33 @@
+﻿
+Console.WriteLine("Write_number_1:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Write_number_2:");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Write_number_3:");
+int c = int.Parse(Console.ReadLine());
+int max = a;
+int min = a;
+if (b>max)
+{
+   max = b;
+}
+else
+{
+    min = b;
+}
+if (c>max)
+{
+    max = c;
+}
+else
+{
+    min = c;
+}
+Console.Write("Max = ");
+Console.WriteLine(max);
+Console.Write("Min = ");
+Console.WriteLine(min);
+
+
+
+
