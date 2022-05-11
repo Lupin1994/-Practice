35405975e5cc28827cfb5d05535f442d7e4603eb
Alex <skorpion_94@list.ru>
2022-05-11 16:48:29 +0300

    Показать числа от -N до N

--- work04/Program.cs
+++ work04/Program.cs
@@ -1,21 +1,21 @@
-﻿// Console.WriteLine("Write_number_1:");
-// int a = int.Parse(Console.ReadLine());
-// Console.WriteLine("Write_number_2:");
-// int b = int.Parse(Console.ReadLine());
-// while (a <= b)
-// {
-//     Console.Write("[");
-//     Console.Write(a);
-//     Console.Write("],");
-//     a++;
-// }
-
-Console.WriteLine("Write_number:");
-int x = int.Parse(Console.ReadLine());
-
-int min = -x;
-while (min <= x)
+﻿Console.WriteLine("Write_number_1:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Write_number_2:");
+int b = int.Parse(Console.ReadLine());
+while (a <= b)
 {
-    Console.Write(" " + min);
-    min++;
+    Console.Write("[");
+    Console.Write(a);
+    Console.Write("],");
+    a++;
 }
+
+// Console.WriteLine("Write_number:");
+// int x = int.Parse(Console.ReadLine());
+
+// int min = -x;
+// while (min <= x)
+// {
+//     Console.Write(" " + min);
+//     min++;
+// }
