c2ac8fbecefc6c85c4cb1a7ca64da9546f8d7c1a
Alex <skorpion_94@list.ru>
2022-05-11 16:33:50 +0300

    Показать числа от -N до N

---
+++ work04/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Write_number_1:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Write_number_2:");
+int b = int.Parse(Console.ReadLine());
+while (a<=b)
+{
+    Console.Write("[");
+    Console.Write(a);
+    Console.Write("],");
+    a++;
+}
