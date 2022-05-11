6b2a8f9e1d8c3270ccb1086c83a7d097060e6879
Alex <skorpion_94@list.ru>
2022-05-11 23:26:39 +0300

    Вывести 3-ю цифру,3-ех значного числа

---
+++ work05/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите 3-ех значное число:");
+int a = int.Parse(Console.ReadLine());
+if ( a > 99 && a<1000)
+{
+    int b = a % 10;
+    Console.Write("3-тья цифра введенного числа:");
+    Console.WriteLine(b);
+}
+else
+{
+    Console.WriteLine("Число не 3-ех значное.");
+}
