8e1ee00a3fc50ac65c7a485d6868f0abbcb8ba1f
Alex <skorpion_94@list.ru>
2022-05-17 00:57:48 +0300

    Семинар_3 Квадраты чисел от 1 до N

---
+++ 03.work_01/Program.cs
@@ -0,0 +1,38 @@
+﻿//.....................................................................................
+// Console.WriteLine("Write your latest number:");
+// int n = int.Parse(Console.ReadLine());
+// int i = 0;
+// int Q = 1;
+// while (i < n)
+// {
+//     i++;
+//     Q = i * i;
+//     Console.Write($" [{i}],");
+//     Console.WriteLine($" [{Q}],");
+// }
+//....................................................................................
+Console.WriteLine("Write number:");
+int max = int.Parse(Console.ReadLine());
+int count = 1;
+while(count <= max)
+{
+    Console.WriteLine(count*count);
+    count++;
+}
+//....................................................................................
+// Console.WriteLine("Write your latest number:");
+// int n = int.Parse(Console.ReadLine());
+// int count = 0;
+// while (count<n)
+// {
+//     int digit = count +1;
+//     Console.WriteLine($"Список квадратов чисел от 1 до {n} {digit*digit}");
+//     count++;
+// }
+// .....................................................................................
+//Console.WriteLine("Введите число n:");
+// int n = int.Parse(Console.ReadLine());
+// for (int i = 1; i <= n; i++)
+// {
+//     Console.Write($"{i * i} , ");
+// }
