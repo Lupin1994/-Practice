b5b49a48cdb5d785ba0624a707d15af991239118
Alex <skorpion_94@list.ru>
2022-05-16 22:11:36 +0300

    Семинар_2 Задача определение четверти на координатной плоскости

---
+++ 02.work_02/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.WriteLine("Write X:");
+int X = int.Parse(Console.ReadLine());
+Console.WriteLine("Write Y:");
+int Y = int.Parse(Console.ReadLine());
+if (X > 0 && Y > 0)
+{
+    Console.WriteLine("First quarter");
+}
+if (X < 0 && Y > 0)
+{
+    Console.WriteLine("Second quarter");
+}
+if (X < 0 && Y < 0)
+{
+    Console.WriteLine("Third quarter");
+}
+if (X > 0 && Y < 0)
+{
+    Console.WriteLine("Fourth quarter");
+}
+
+// if (X > 0)
+// {
+//     if (Y > 0)
+//     {
+//         Console.WriteLine("First quarter");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Fourth quarter");
+//     }
+// }
+// else
+// {
+//     if (Y > 0)
+//     {
+//         Console.WriteLine("Second quarter");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Third quarter");
+//     }
+// }
