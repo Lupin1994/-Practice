b2eaa74747e284de79ce5d7248fe05fbb88c13d0
Alex <skorpion_94@list.ru>
2022-05-16 21:36:50 +0300

    Семинар_2 Задача кратно ли число 7 и 23

---
+++ 02.work_01/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Write your number:");
+int number = int.Parse(Console.ReadLine());
+
+if (number%7==0 && number%23==0)
+{
+    Console.WriteLine($"{number} is multiple of 7 and 23");
+}
+else
+{
+    Console.WriteLine($"{number} isn't multiple of 7 and 23");
+}
