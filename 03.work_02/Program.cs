465739126044a58ebff5a9940eff2800a1e06047
Alex <skorpion_94@list.ru>
2022-05-17 12:00:40 +0300

    Семинар_3 Найти сумму чисел от 1 до A

---
+++ 03.work_02/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Write your latest number:");
+int a = int.Parse(Console.ReadLine());
+int max = a;
+int i = 0;
+int inew = 0;
+while (i <= max)
+{
+    inew = inew + i;
+    Console.WriteLine(inew);
+    i++;
+}
+// Console.WriteLine("Write your latest number:");
+// int a = int.Parse(Console.ReadLine());
+// int sum = a*(a+1)/2; // математическая формула по нахождению ариф прогрессии
+// Console.WriteLine(sum);
