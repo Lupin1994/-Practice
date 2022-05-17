7ef8d1fffb1290edc27412160d24ea01937e7738
Alex <skorpion_94@list.ru>
2022-05-17 12:54:17 +0300

    Семинар_3 Вычислить произведение чисел от 1 до N

---
+++ 03.work_04/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Write your number:");
+int N = int.Parse(Console.ReadLine());
+int i = 1;
+double Mult = 1;
+while (i <= N)
+{
+    Mult = Mult * i;
+    i++;
+    Console.WriteLine(Mult);
+}
+Console.WriteLine($"Your result is {Mult}");
+
+// Console.WriteLine("Write your N:");
+// int N = int.Parse(Console.ReadLine());
+// double p = 1;
+// for (int i = 1; i<=N;i++)
+// {
+//     p=p*i;
+// }
+// Console.WriteLine($"Your result is {p}");
