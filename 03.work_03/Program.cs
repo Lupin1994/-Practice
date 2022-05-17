a0bc039965b6ae73a50c2cde34eeac64c685653a
Alex <skorpion_94@list.ru>
2022-05-17 12:29:10 +0300

    Семинар_3 Найти количество цифр в числе

---
+++ 03.work_03/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Write your number:");
+int a = int.Parse(Console.ReadLine());
+int i = 0;
+while (a > 0)
+{
+    a = a / 10;
+    i++;
+}
+Console.WriteLine("Колличество цифр:" + i);
