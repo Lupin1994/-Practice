208b14624d3962bdabc5e167cb92eec5c6cea158
Alex <skorpion_94@list.ru>
2022-05-19 11:21:30 +0300

    Сложить нечетные позиции чисел в массиве( програмка по рандомной генерации массиваgit add 04.work_03

---
+++ 04.work_03/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите размер массива: ");
+
+int Number = int.Parse(Console.ReadLine() ?? "0");
+int[] arr = new int[Number];
+
+for (int i = 0; i < Number; i++)
+{
+    arr[i] = new Random().Next(0, 10);
+    Console.Write($"{arr[i]} ");
+}
+Console.WriteLine();   
+
+int Sum = 0;
+
+for(int i = 0; i<arr.Length;i++)
+{
+    if (i % 2 == 1)
+    {
+        Sum = Sum + arr[i];
+    } 
+}
+Console.Write(Sum);
