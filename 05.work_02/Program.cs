e5e52a365619c7de48208d6fd519407919d1b5e5
Alex <skorpion_94@list.ru>
2022-06-02 14:46:20 +0300

    Являются ли числа сторонами треугольника

---
+++ 05.work_02/Program.cs
@@ -0,0 +1,18 @@
+﻿//  Являются ли три числа сторонами треугольника
+// a,b,c, являются треугольником если a+b>c и b+c>a и a+c>b
+Console.WriteLine("Write number a: ");
+int A = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Write number B: ");
+int B = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Write number C: ");
+int C = int.Parse(Console.ReadLine() ?? "0");
+if (A + B > C && B + C > A && A + C > B)
+{
+    Console.WriteLine("This is triangle");
+}
+else
+{
+    Console.WriteLine("This is not triangle");
+}
