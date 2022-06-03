ca4b04c71040d9907a8855d7c7a98ceaabce7476
Alex <skorpion_94@list.ru>
2022-06-03 13:34:02 +0300

    Показать число фибоначчи меньше заданного N

---
+++ 05.work_04/Program.cs
@@ -0,0 +1,16 @@
+﻿// Показать число фибоначчи меньше заданного числа N
+Console.WriteLine("Write N:");
+int N = int.Parse(Console.ReadLine() ?? "0");
+
+int FirstValue = 0;
+int SecondValue = 1;
+int result = 0;
+//Console.Write($"{FirstValue} {SecondValue}");
+
+while (FirstValue+SecondValue < N)
+{
+    result = FirstValue + SecondValue;
+    Console.Write(result + " ");
+    FirstValue = SecondValue;
+    SecondValue = result;
+}
