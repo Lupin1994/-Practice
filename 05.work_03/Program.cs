41c85cdc42ee33b9c46c497cda0ddab9cd7cc338
Alex <skorpion_94@list.ru>
2022-06-03 12:40:03 +0300

    Программа по переводу с 10 в двоичную систему

---
+++ 05.work_03/Program.cs
@@ -0,0 +1,86 @@
+﻿// Написать программу преобразования десятичного числа в двоичное
+// {
+//     int count = col.Length;
+//     int position = 0;
+//     while(position<count)
+//     {
+//         Console.Write(col[position]);
+//         position++;
+//     }
+// }
+// Console.WriteLine("Number : ");
+// int Num = int.Parse(Console.ReadLine() ?? "0");
+// int tmpnumber = Num;
+// int tmp = 0;
+// int count = 0;
+// void FillArray(int[] array)
+// {
+//     int tmp = 0;
+//     int index = 0;
+//     int length = array.Length;
+//     while(index<length)
+//     {
+//         tmp = Num%2;
+//         Num= Num/2;
+//         array[index]=tmp;
+//         index++;
+//     }
+// }
+// void PrintArray(int[] col )
+// {
+//     int count = col.Length;
+//     int position = 0;
+//     while(position<count)
+//     {
+//         Console.Write(col[position]);
+//         position++;
+//     }
+// }
+// while(tmpnumber>0)
+// {
+//     tmp = tmpnumber%2;
+//     tmpnumber=tmpnumber/2;
+//     count++;
+// }
+// int[] array = new int[count];
+// FillArray(array);
+// PrintArray(array.Reverse().ToArray());
+
+
+// Console.WriteLine("Write number: ");
+// int Num = int.Parse(Console.ReadLine() ?? "0");
+// int j = 0;
+// int[] arr = new int[16];
+// int res = 0;
+// for (int i = 0; Num > 0; i++)
+// {
+//     res = Num % 2;
+//     Num = Num / 2;
+//     arr[i] = res;
+//     Console.Write($"{arr[i]} ,");
+//     j = i;
+//     //Console.WriteLine();   
+// }
+// int count = 1;
+// foreach(int k in arr)
+// { 
+//     Console.WriteLine($"{count} element array - {k}");
+//     count++;
+// }
+
+Console.Write("Write number : ");
+int num = int.Parse(Console.ReadLine()??"0");
+string result = "";
+string RevResult = "";
+while (num>0)
+{
+    result =result+(num%2).ToString();
+    num = num/2;
+}
+int length=result.Length;
+for(int i = length -1; i>=0;i--)
+{
+    RevResult += result[i];
+
+}
+Console.WriteLine($"{RevResult}");
