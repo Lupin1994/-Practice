0c2a7b4c3b2d2d47dd395bef4d3db9c05a80bafc
Alex <skorpion_94@list.ru>
2022-06-08 13:29:59 +0300

    Двумерный массив n*k с заменой элементов на противоположный

---
+++ 06.work_02/Program.cs
@@ -0,0 +1,40 @@
+﻿// В двумерном массиве n×k заменить четные элементы на противоположные
+
+void PrintMatrix(int[,] matr)
+{
+    for (int n = 0; n < matr.GetLength(0); n++)
+    {
+        for (int k = 0; k < matr.GetLength(1); k++)
+        {
+            Console.Write($"{matr[n, k]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void FillArray(int[,] matr)
+{
+    for (int n = 0; n < matr.GetLength(0); n++)
+    {
+        for (int k = 0; k < matr.GetLength(1); k++)
+        {
+            matr[n, k] = new Random().Next(1, 10);
+        }
+    }
+}
+void ReplacementMatrix(int[,] matr)
+{
+    for (int n = 0; n < matr.GetLength(0); n++)
+    {
+        for (int k = 0; k < matr.GetLength(1); k++)
+        {
+            if (matr[n, k] % 2 == 0) matr[n, k] = -matr[n, k];
+        }
+    }
+}
+int[,] matrix = new int[5, 5];
+FillArray(matrix);
+Console.WriteLine();
+PrintMatrix(matrix);
+Console.WriteLine();
+ReplacementMatrix(matrix);
+PrintMatrix(matrix);
