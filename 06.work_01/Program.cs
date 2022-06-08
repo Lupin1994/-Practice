be6456ef769651397ec0fd9e3afef509ecd5296f
Alex <skorpion_94@list.ru>
2022-06-08 13:29:16 +0300

    Двумерный массив m*n

--- 06.work_01/Program.cs
+++ 06.work_01/Program.cs
@@ -24,3 +24,18 @@ int[,] matrix = new int[3,6];
 FillArray(matrix);
 Console.WriteLine();
 PrintArray(matrix);
+
+// Console.Write("Write number row in matrix: ");
+// int m = int.Parse(Console.ReadLine() ?? "0");
+// Console.Write("Write number column in matrix: ");
+// int n = int.Parse(Console.ReadLine() ?? "0");
+// int[,] matr = new int[m, n];
+// for (int i = 0; i < m; i++)
+// {
+//     for (int j = 0; j < n; j++)
+//     {
+//         matr[i, j] = new Random().Next(1, 10);
+//         Console.Write($"{matr[i , j]} ");
+//     }
+//     Console.WriteLine();
+// }
