dad127877ea42b385ff09633e4b46a7298a51dbc
Alex <skorpion_94@list.ru>
2022-06-08 12:19:11 +0300

    Двумерный массив m*n

---
+++ 06.work_01/Program.cs
@@ -0,0 +1,26 @@
+﻿// Показать двумерный массив размером m×n заполненный целыми числами
+void PrintArray(int[,] matr)
+{
+    for (int m = 0; m < matr.GetLength(0); m++)
+    {
+        for (int n = 0; n < matr.GetLength(1); n++)
+        {
+            Console.Write($"{matr[m,n]} ");
+        }
+    Console.WriteLine();    
+    }
+}
+void FillArray(int[,] matr)
+{
+    for (int m = 0; m < matr.GetLength(0); m++)
+    {
+        for (int n = 0; n < matr.GetLength(1); n++)
+        {
+            matr[m,n] = new Random().Next(0,10);
+        }   
+    }
+}
+int[,] matrix = new int[3,6];
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
